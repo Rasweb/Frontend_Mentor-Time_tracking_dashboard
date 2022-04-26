df93a4771a408c359fc37be0fecfb822e9f989b2
RasWeb <rasmuspalm@fastmail.se>
2022-04-26 16:53:59 +0200

    fetch function

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,9 @@
 //   });
 
 // Fetch template
+// The fetch will return a promise.
+// The then will run the JSON data when it's fetched.
+// The catch function will run if there are any errors.
 // file/url
 // fetch()
 //   .then(function (response) {
@@ -18,3 +21,34 @@
 //   .catch(function (err) {
 //     // The error
 //   });
+
+// The fetch code
+// fetching the data.json file
+fetch("data.json")
+  // Runs the response
+  .then(function (response) {
+    // To extract JSON data the function runs json()
+    // .json() returns a promise
+    return response.json();
+  })
+  // Retrives the JSON data as a parameter
+  .then(function (data) {
+    // This function will append the JSON to html.
+    appendData(data);
+  })
+  // logs the error message to the console.
+  .catch(function (err) {
+    console.log("error" + err);
+  });
+
+function appendData(data) {
+  // Finds the div
+  let dataContainer = document.getElementById("dataDiv");
+
+  // Appends the data to the page
+  for (let index = 0; index < data.length; index++) {
+    let div = document.createElement("div");
+    div.innerHTML = "Title" + data[i].title;
+    dataContainer.appendChild(div);
+  }
+}
