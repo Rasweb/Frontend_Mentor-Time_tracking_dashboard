584cc7e294706f55f3bd018e9e15ae2af42d1117
RasWeb <rasmuspalm@fastmail.se>
2022-04-26 16:34:57 +0200

    Some fetch basics

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+// fetch("data.json")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     appendData(data);
+//   })
+//   .catch(function (err) {
+//     console.log("error" + err);
+//   });
+
+// Fetch template
+// file/url
+// fetch()
+//   .then(function (response) {
+//     // The JSON data.
+//   })
+//   .catch(function (err) {
+//     // The error
+//   });
